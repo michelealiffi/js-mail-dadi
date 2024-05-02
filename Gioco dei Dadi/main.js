472f1bb220d6c11b4bc4321625f5d0382b304045
michelealiffi <michelealiffi890@gmail.com>
2024-05-02 14:33:04 +0200

    Finish Gioco dei Dadi

--- Gioco dei Dadi/main.js
+++ Gioco dei Dadi/main.js
@@ -1,1 +1,20 @@
 'use strict'
+
+let userNumber = Math.floor(Math.random() * (0, 6) + 1);
+let pcNumber = Math.floor(Math.random() * (0, 6) + 1);
+
+debugger
+
+while(userNumber == pcNumber) {
+    userNumber = Math.floor(Math.random() * (0, 6) + 1);
+    pcNumber = Math.floor(Math.random() * (0, 6) + 1);
+}
+
+console.log(userNumber);
+console.log(pcNumber);
+
+if(userNumber > pcNumber) {
+    console.log('Vince il giocatore');
+} else {
+    console.log('Vince il computer');
+}
