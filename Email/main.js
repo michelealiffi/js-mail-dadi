c2a2deb1a6b7f1f885336fbb9b430a5d1191595b
michelealiffi <michelealiffi890@gmail.com>
2024-05-02 17:31:54 +0200

    Finish email

--- Email/main.js
+++ Email/main.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const userEmail = prompt('Inserisci la tua Email');
+console.log(userEmail);
+
+const emailList = ["gigi.d.alessio@libero.it", "carapace229@email.it", "gmail@michele.com", 'pacchero'];
+
+
+if(emailList.includes(userEmail)) {
+    console.log("La tua email è " + userEmail +  " puoi accedere alla pagina");
+} else {
+    console.log("La tua email è " + userEmail + " non è in lista, quindi non può entrare");
+}
